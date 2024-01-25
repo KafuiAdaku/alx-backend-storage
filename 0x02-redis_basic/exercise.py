eeb7ffc772c23f93f008af2c7c21e17a530fb1d8
Dennis Adaku <dennisadaku@ymail.com>
2024-01-25 12:46:08 +0000

    added solution to task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,19 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+
+def count_calls(method: Callable) -> Callable:
+    """My decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -11,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Returns a string"""
         rand_key = str(uuid.uuid4())
